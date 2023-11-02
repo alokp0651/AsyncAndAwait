e37aa618ed98c24b6fd467e89ef22255cf11d8d5
alokd <alokd@KNOXY>
2023-11-02 12:23:38 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AsyncAndAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProcessFile = new System.Windows.Forms.Button();
+            this.lblcount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ProcessFile
+            // 
+            this.ProcessFile.Location = new System.Drawing.Point(190, 59);
+            this.ProcessFile.Name = "ProcessFile";
+            this.ProcessFile.Size = new System.Drawing.Size(407, 66);
+            this.ProcessFile.TabIndex = 0;
+            this.ProcessFile.Text = "Process File";
+            this.ProcessFile.UseVisualStyleBackColor = true;
+            this.ProcessFile.Click += new System.EventHandler(this.ProcessFile_Click);
+            // 
+            // lblcount
+            // 
+            this.lblcount.AutoSize = true;
+            this.lblcount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcount.Location = new System.Drawing.Point(199, 202);
+            this.lblcount.Name = "lblcount";
+            this.lblcount.Size = new System.Drawing.Size(0, 16);
+            this.lblcount.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblcount);
+            this.Controls.Add(this.ProcessFile);
+            this.Name = "Form1";
+            this.Text = "Count Charachters in this file";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ProcessFile;
+        private System.Windows.Forms.Label lblcount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAndAwait
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int CountCharacters()
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader("c:\\Data\\Data.txt"))
+            { 
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
+        }
+        /// <summary>
+        ///this is the normal click event of the method in which we cant perform another process on the form because of CountCharachter method because it is processing for 5 sec after its processing time is over after that only we can change the size or move the form .
+        /// </summary>
+
+        //private void ProcessFile_Click(object sender, EventArgs e)
+        //{
+        //    lblcount.Text = "Processing file.please wait";
+        //    int Count = CountCharacters();
+        //    lblcount.Text = Count.ToString() + " charaters in file";
+        //}
+
+
+        private async void ProcessFile_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountCharacters);
+            task.Start();
+
+            lblcount.Text = "Processing file.please wait";
+            int Count = await task;
+            lblcount.Text = Count.ToString() + " charaters in file";
+        }
+        //   When we use the async MethodAttributes then we do the multiple task at the same time 
+    }
+}
